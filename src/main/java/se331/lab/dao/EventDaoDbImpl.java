532405d7999870d09cb23f7f46ff580238b64d71
Evan Clark <black0ashes@gmail.com>
2025-09-11 17:47:24 +0700

    (2.3) quasi

---
+++ src/main/java/se331/lab/dao/EventDaoDbImpl.java
@@ -0,0 +1,34 @@
+package se331.lab.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Event;
+import se331.lab.repository.EventRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class EventDaoDbImpl implements EventDao {
+    final EventRepository eventRepository;
+    @Override
+    public Integer getEventSize() {
+        return Math.toIntExact(eventRepository.count());
+    }
+
+    @Override
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        List<Event> events = eventRepository.findAll();
+        pageSize = pageSize == null ? events.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page - 1) * pageSize;
+        List<Event> output = events.subList(firstIndex, firstIndex + pageSize);
+        return output;
+    }
+
+    @Override
+    public Event getEvent(Long id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+}
