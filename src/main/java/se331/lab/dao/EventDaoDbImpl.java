883331cfeea293b68903f65d6988faddc4039d47
Evan Clark <black0ashes@gmail.com>
2025-09-25 19:33:27 +0700

    lab9 V (1.8)

--- src/main/java/se331/lab/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/dao/EventDaoDbImpl.java
@@ -43,7 +43,7 @@ public class EventDaoDbImpl implements EventDao {
 
     @Override
     public Page<Event> getEvents(String title,Pageable page) {
-        return eventRepository.findByTitleContainingAndDescriptionContaining(title, title , page);
+        return eventRepository.findByTitleContainingOrDescriptionContainingOrOrganizerContaining(title, title, title, page);
     }
 
 }
