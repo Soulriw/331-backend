62fb0a952b017de1a55073f8b7e57dcdb1c7d1bf
Evan Clark <black0ashes@gmail.com>
2025-10-10 05:13:15 +0700

    l10 I (1.2)

--- src/main/java/se331/lab/dao/EventDao.java
+++ src/main/java/se331/lab/dao/EventDao.java
@@ -1,6 +1,7 @@
 package se331.lab.dao;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import se331.lab.entity.Event;
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface EventDao {
     Page<Event> getEvents(Integer pageSize, Integer page);
     Event getEvent(Long id);
     Event save(Event event);
+    Page<Event> getEvents(String name, Pageable page);
 }
--- src/main/java/se331/lab/service/EventServiceImpl.java
+++ src/main/java/se331/lab/service/EventServiceImpl.java
@@ -3,6 +3,7 @@ package se331.lab.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import se331.lab.dao.EventDao;
 import se331.lab.dao.OrganizerDao;
@@ -40,5 +41,10 @@ public class EventServiceImpl implements EventService {
         event.setOrganizer(organizer);
         organizer.getOwnEvents().add(event);
         return eventDao.save(event);
+    }
+
+    @Override
+    public Page<Event> getEvents(String title, Pageable pageable) {
+        return eventDao.getEvents(title, pageable);
     }
 }
