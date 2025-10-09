646cf294c5ce3ddf223170fe7b2757fcca439b7e
Evan Clark <black0ashes@gmail.com>
2025-10-10 02:31:37 +0700

    l9 VII (6.1)

---
+++ src/main/java/se331/lab/controller/OrganizerController.java
@@ -0,0 +1,17 @@
+package se331.lab.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se331.lab.service.OrganizerService;
+
+@RestController
+@RequiredArgsConstructor
+public class OrganizerController {
+    final OrganizerService organizerService;
+    @GetMapping("/organizers")
+    ResponseEntity<?> getOrganizers() {
+        return ResponseEntity.ok(organizerService.getAllOrganizer());
+    }
+}
+
---
+++ src/main/java/se331/lab/dao/OrganizerDaoImpl.java
@@ -0,0 +1,19 @@
+package se331.lab.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Organizer;
+import se331.lab.repository.OrganizerRepository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrganizerDaoImpl implements OrganizerDao {
+    final OrganizerRepository organizerRepository;
+
+    @Override
+    public Page<Organizer> getOrganizer(Pageable pageable) {
+        return organizerRepository.findAll(pageable);
+    }
+}
