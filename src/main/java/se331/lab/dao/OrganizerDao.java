134669e52e2ed86f4a2bc4be07a17b4e04d9b07f
Evan Clark <black0ashes@gmail.com>
2025-10-10 03:38:06 +0700

    l9 X (6.5)

--- src/main/java/se331/lab/dao/OrganizerDao.java
+++ src/main/java/se331/lab/dao/OrganizerDao.java
@@ -8,6 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import se331.lab.entity.Organizer;
 
+import java.util.Optional;
+
 public interface OrganizerDao {
-    Page<Organizer> getOrganizer(Pageable pageable);
+    Page<Organizer> getOrganizer(Pageable pageRequest);
+    Optional<Organizer> findById(Long id);
 }
