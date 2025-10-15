bca18d76d856a2123825423e1d9096237ba7a5c0
Evan Clark <black0ashes@gmail.com>
2025-10-16 03:27:17 +0700

    l10 VI (1.9)

--- src/main/java/se331/lab/repository/EventRepository.java
+++ src/main/java/se331/lab/repository/EventRepository.java
@@ -12,5 +12,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     Page<Event> findByTitle(String title, Pageable pageRequest);
     Page<Event> findByTitleContaining(String title, Pageable pageRequest);
     Page<Event> findByTitleContainingAndDescriptionContaining(String title, String description, Pageable pageRequest);
-    Page<Event> findByTitleContainingOrDescriptionContainingOrOrganizer_NameContaining(String title, String description, String organizerName, Pageable oageRequest);
+    Page<Event> findByTitleIgnoreCaseContainingOrDescriptionIgnoreCaseContainingOrOrganizer_NameIgnoreCaseContaining(String title, String description, String organizerName, Pageable oageRequest);
 }
