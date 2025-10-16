c4a2a0ed9575544607fa3a68c3996db8ae0a2c2d
Evan Clark <black0ashes@gmail.com>
2025-10-16 20:38:02 +0700

    l11 I (3.4)

---
+++ src/main/java/se331/lab/controller/SupabaseController.java
@@ -0,0 +1,29 @@
+package se331.lab.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import se331.lab.util.SupabaseStorageService;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class SupabaseController {
+    final SupabaseStorageService supabaseStorageService;
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try{
+            String fileUrl = supabaseStorageService.uploadFile(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (Exception e){
+            return ResponseEntity.status(500).body("Error uploading file: " + e.getMessage());
+        }
+    }
+}
